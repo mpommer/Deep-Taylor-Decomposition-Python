64ea347109435577848aa45e3ca3286be1761726
mpommer <marcelpommer@web.de>
2022-07-10 10:13:31 +0200

    deep taylor decomposition

--- PythonFiles/taylor_decomposition_presentation.py
+++ PythonFiles/taylor_decomposition_presentation.py
@@ -10,12 +10,12 @@ from PythonFiles.omega_squared import omega_squared
 from PythonFiles.Neural_Network import NN
 
 # preprocess data
-x_train, y_train, x_test, y_test, feature_name = data_reading_transformation(path_url = "data/titanic", minmaxscaler = False, withID = True)
+x_train, y_train, x_test, y_test, feature_name = data_reading_transformation(path_url = "data/titanic", minmaxscaler = True, withID = True)
 
 # build the neural network
 neural_network = NN(x_train, y_train)
-hidden_layers = 2
-neural_network.fit(layers = hidden_layers, neurons = [10]*hidden_layers, verbose = 0)
+hidden_layers = 1
+neural_network.fit(layers = hidden_layers, neurons = [5]*hidden_layers, verbose = 0)
 
 # print test results
 neural_network.testResult(x_test,y_test)
